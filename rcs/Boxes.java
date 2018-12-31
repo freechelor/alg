c8a36d24d00ed5f2117aaa061bc0db730512460e
freechelor <jasoninus.choi@gmail.com>
2018-12-31 19:58:23 +0000

    1st

---
+++ rcs/Boxes.java
@@ -0,0 +1,129 @@
+package alg.rcs;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+public class Boxes {
+	static int max = 0;
+	static List<Box> getBoxes() {
+		List<Box> list = new ArrayList<>();
+		list.add(new Box(11,45,15));
+		list.add(new Box(17,27,33));
+		list.add(new Box(26,38,25));
+		list.add(new Box(28,44,33));
+		list.add(new Box(34,16,56));
+		list.add(new Box(22,57,81));
+		list.add(new Box(44,88,70));
+		list.add(new Box(71,100,99));
+		return list;
+	}
+
+	static boolean checkValid(Box n, Box p) {
+		if(p==null) return true;
+		if(n.width<p.width&&n.depth<p.depth) return true;
+		return false;
+	}
+/*
+	static int getMax(List<Box> list, int idx, Box prev) {
+		for(int i = idx; i < list.size(); i++) {
+			Box cur = (Box)list.get(i);
+			if(checkValid(cur, prev)) {
+				int v1 = getMax(list, i+1, prev);
+				int v2 = cur.height+getMax(list, i+1, cur);
+				max = Math.max(v1, v2);
+			}
+		}
+		return max;
+	}
+*/
+
+	static int getMax(List<Box> list, int idx, Box prev) {
+		Box cur = null;
+		if(idx<list.size()) cur = (Box)list.get(idx);
+		else return max;
+		if(checkValid(cur, prev)) {
+			int v1 = getMax(list, idx+1, prev);
+			int v2 = cur.height+getMax(list, idx+1, cur);
+			max = Math.max(v1, v2);
+		} else {
+			max += getMax(list, idx+1, prev); 
+		}
+		return max;
+	}
+
+	public static void main(String[] args) {
+		// 1. first sort with one of dimensions
+		List<Box> list = getBoxes();
+		list.forEach(u->System.out.println(u));
+		list.sort((Box a, Box b) -> b.height-a.height);
+		System.out.println();
+		list.forEach(u->System.out.println(u));
+
+		System.out.println();
+		// 2. check if one can be on the top of the previous one
+		// if ok, add it to result list, if not, skip because anyway you can not chnage the order already sorted in height
+		System.out.println(getMax(list, 0, null));
+
+/*		System.out.println();
+		System.out.println("Sort1");
+		List<Box> list = getBoxes();
+		for( Box b : list ) {
+			System.out.println(b);
+		}
+		list.forEach(b->System.out.println(b));
+
+		Collections.sort(list, new Comparator<Box>(){
+			public int compare(Box a, Box b) {
+				return a.height - b.height;
+			}
+		});
+
+		list.forEach(b->System.out.println(b));
+
+		System.out.println();
+		System.out.println("Sort2");
+		list = getBoxes();
+		list.forEach(b->System.out.println(b));
+		list.sort(new Comparator<Box>() {
+			public int compare(Box a, Box b) {
+				return a.height - b.height;
+			}
+		});
+		list.forEach(b->System.out.println(b));
+
+		System.out.println();
+		System.out.println("Sort3");
+
+		list = getBoxes();
+		list.forEach(b->System.out.println(b));
+		list.sort((Box a, Box b) -> (a.height-b.height));
+		list.forEach(b->System.out.println(b));*/
+	}
+}
+
+class MyComparator implements Comparator<Box> {
+	public int compare(Box a, Box b) {
+		return a.height - b.height;
+	}
+}
+
+class Box implements Comparator<Box> {
+	public int height;
+	public int width;
+	public int depth;
+
+	public Box(int h, int w, int d) {
+		height = h;
+		width = w;
+		depth = d;
+	}
+
+	public int compare(Box a, Box b) {
+		return a.height - b.height;
+	}
+
+	public String toString() {
+		return ("height : " + height + " , width : " + width + ", depth : " + depth);
+	}
+}
