4acd41db04bc4c7e366786e8be656ccf8d9d99a6
freechelor <jasoninus.choi@gmail.com>
2019-01-23 21:11:27 +0000

    1st

---
+++ q/HeteroQ.java
@@ -0,0 +1,36 @@
+/**
+ * TODO : 
+ * 1. Time/Space Complexity Analysis
+ * 		BUT, MOST OF ALL, IT IS IMPORTANT TO 1.1. LIST POSSIBLE/AVAILABLE SOLUTIONS AND 1.2. THINK ABOUT COMPLEXITY FIRST AND IN ADVANCE,
+ * 		1.3. START WITH BEST COMPLEXITY SOLUTION
+ * 2. Improve Algorithm/Performance
+ * 3. Learn unfamiliar syntax 
+ * 4. Any other ways to consider + compare in terms of Time/Space complexity
+ *
+ */
+package alg.q;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+import java.util.Stack;
+import java.util.Queue;
+
+public class HeteroQ {
+	//method1. hold 3 different kinds of q.
+	//one only holds cats, another only holds dogs, and the last holds both of them
+	//TC : O(1), SC : O(2N) -> O(N)
+	//if memory is not in so critical condition, this could be a solution
+
+
+
+
+	//method2. in case of lack of memory, BF:
+	// just hold one queue for every data.
+	// and every time it gets request, scan from the first element sequentially
+	// TC : 
+
+	public static void main(String[] args) {
+	}
+}
