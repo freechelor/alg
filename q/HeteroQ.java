0b3eb0a628c804740f847a3c7d3aa23edd9ca4a5
freechelor <jasoninus.choi@gmail.com>
2019-01-25 21:27:32 +0000

    mod

--- q/HeteroQ.java
+++ q/HeteroQ.java
@@ -31,6 +31,58 @@ public class HeteroQ {
 	// and every time it gets request, scan from the first element sequentially
 	// TC : 
 
-	public static void main(String[] args) {
+	LinkedList<Animal> list = new LinkedList<>();
+	public void add(Animal a) {
+		list.add(a);
 	}
+
+	public 
+
+	public static void main(String[] args) {
+		Dog d1 = new Dog();
+		Dog d2 = new Dog();
+		Cat c1 = new Cat();
+		Cat c2 = new Cat();
+		Cat c3 = new Cat();
+		Cat c4 = new Cat();
+		Cat c5 = new Cat();
+		Cat c6 = new Cat();
+		Node n1 = new Node(d1);
+		Node n2 = new Node(d2);
+		Node n3 = new Node(c1);
+		Node n4 = new Node(c2);
+		Node n5 = new Node(c3);
+		Node n6 = new Node(c4);
+		Node n7 = new Node(c5);
+		Node n8 = new Node(c6);
+		n1.next = n2;
+		n2.next = n3;
+		n3.next = n4;
+		n4.next = n5;
+		n5.next = n6;
+		n6.next = n7;
+		n7.next = n8;
+
+		HeteroQ q = new HeteroQ();
+		q.add(n1);
+	}
+}
+
+class Node {
+	public Animal ani;
+	public Animal next;
+	public Node(Animal ani) {
+		this.ani = ani;
+	}
+}
+
+class Animal {
+}
+
+class Dog extends Animal {
+	public final string type = "Dog";
+}
+
+class Cat extends Animal {
+	public final string type = "Cat";
 }
