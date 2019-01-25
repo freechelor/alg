caed99cf01516003425199f9f525dec727d3c864
freechelor <jasoninus.choi@gmail.com>
2019-01-25 21:44:53 +0000

    Animal.java

---
+++ stck/run.sh
@@ -0,0 +1,1 @@
+java -cp /usr/bin alg.stck.${1}
---
+++ stx/oop/IB.java
@@ -0,0 +1,23 @@
+/**
+ * TODO : 
+ * 1. Time/Space Complexity Analysis
+ * 		BUT, MOST OF ALL, IT IS IMPORTANT TO 1.1. LIST POSSIBLE/AVAILABLE SOLUTIONS AND 1.2. THINK ABOUT COMPLEXITY FIRST AND IN ADVANCE,
+ * 		1.3. START WITH BEST COMPLEXITY SOLUTION
+ * 2. Improve Algorithm/Performance
+ * 3. Learn unfamiliar syntax 
+ * 4. Any other ways to consider + compare in terms of Time/Space complexity
+ *
+ */
+package alg.stx.oop;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+import java.util.Stack;
+import java.util.Queue;
+
+public interface IB {
+	void move();
+	void eat();
+}
