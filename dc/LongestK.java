6fb5b44f090e1ed7acb820167e81875d2b6b1f85
freechelor <jasoninus.choi@gmail.com>
2019-05-01 14:44:57 +0000

    2nd

--- dc/LongestK.java
+++ dc/LongestK.java
@@ -26,43 +26,42 @@ the longest substring with k distinct characters is "bcb".*/
 
 public class LongestK {
 	Hashtable<Character,Integer> tbl = new Hashtable<>();
-	int max = 0;	
+	int cur = 0;	
 	int rMax = Integer.MIN_VALUE;
 	int usedC = 0;
 	Vector<Character> v = new Vector<>(); // to change vector to any other kind of class
 	Character first = null;
 
 	public int getLongest(String s, int k) {
-		if(s==null) return max;
+		if(s==null) return cur;
 		char[] c = s.toCharArray();
 
-		first = c[0];
-		System.out.println("INPUT : " + s);
+//		System.out.println("INPUT : " + s);
 		for(int i = 0; i < c.length; i++) {
-			System.out.println("adding : " + c[i]);
+//			System.out.println("adding : " + c[i]);
 			v.add(c[i]);
 			if(tbl.get(c[i])!=null) {
 				tbl.put(c[i],tbl.get(c[i])+1);
-				max++;
-				if(max>rMax) {
-					rMax = max;
-					System.out.println("=================== FROM " + v.elementAt(0) + " to " + v.elementAt(v.size()-1) +
-							" length : " + rMax);
+				cur++;
+				if(cur>rMax) {
+					rMax = cur;
+//					System.out.println("=================== FROM " + v.elementAt(0) + " to " + v.elementAt(v.size()-1) +
+//							" length : " + rMax);
 				}
 			} else if(tbl.get(c[i])==null) {
 				if(usedC<k) {
 					tbl.put(c[i], 1);
-					max++;
-					if(max>rMax) {
-						rMax = max;
-						System.out.println("=================== FROM " + v.elementAt(0) + " to " + v.elementAt(v.size()-1) + " length : " + rMax);
+					cur++;
+					if(cur>rMax) {
+						rMax = cur;
+//						System.out.println("=================== FROM " + v.elementAt(0) + " to " + v.elementAt(v.size()-1) + " length : " + rMax);
 					}
 					usedC++;
 				} else {
-					System.out.println("before entering : " + first);
+//					System.out.println("before entering : " + first);
 					while(usedC>=k) {
 						first = v.remove(0);
-						System.out.println("In while loop : " + first + " , table size : " + tbl.size());
+//						System.out.println("In while loop : " + first + " , table size : " + tbl.size());
 						int cnt = tbl.get(first)-1;
 						if(cnt>0)
 							tbl.put(first, cnt);
@@ -70,21 +69,21 @@ public class LongestK {
 							tbl.remove(first);
 							usedC--;
 						}
-						max--;
+						cur--;
 					}
 					tbl.put(c[i],1);
-					max++;
+					cur++;
 					usedC++;
 				}
 			}
 		}
-		int vs = v.size();
-		//System.out.println(v.elementAt(0) + " : " + v.elementAt(vs-1));
 		return rMax;
 	}
 
 	public static void main(String[] args) {
 		System.out.println(new LongestK().getLongest("abcbbbbrrqsboooa",2));
 		System.out.println(new LongestK().getLongest("abcbbbbrrqsboooa",3));
+		System.out.println(new LongestK().getLongest("abcbbbbrrqqqqqqqqqqqqqqqsboooa",3));
+		System.out.println(new LongestK().getLongest("aaaabbbaaaabbbbccbbbccbbbbrrqqqqqqqqqqqqqqqsboooa",3));
 	}
 }
