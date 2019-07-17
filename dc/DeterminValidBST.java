94d5139c3be7789b675ebefcc5f6a62de5f7161c
freechelor <jasoninus.choi@gmail.com>
2019-07-17 16:07:03 +0000

    1st

---
+++ dc/DeterminValidBST.java
@@ -0,0 +1,33 @@
+/**
+ * TODO : 
+ * 1. Time/Space Complexity Analysis
+ * 		BUT, MOST OF ALL, IT IS IMPORTANT TO 1.1. LIST POSSIBLE/AVAILABLE SOLUTIONS AND 1.2. THINK ABOUT COMPLEXITY FIRST AND IN ADVANCE,
+ * 		1.3. START WITH BEST COMPLEXITY SOLUTION
+ * 2. Improve Algorithm/Performance
+ * 3. Learn unfamiliar syntax 
+ * 4. Any other ways to consider + compare in terms of Time/Space complexity
+ *
+ */
+package alg.;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+import java.util.Stack;
+import java.util.Queue;
+
+/**
+DC#89_Medium
+This problem was asked by LinkedIn.
+
+Determine whether a tree is a valid binary search tree.
+
+A binary search tree is a tree with two children, left and right, 
+and satisfies the constraint that the key in the left child must be less than or equal to the root 
+and the key in the right child must be greater than or equal to the root.
+**/
+public class DeterminValidBST {
+	public static void main(String[] args) {
+	}
+}
