6418c709418a51c90950214c046ddb4ec17c8a8d
freechelor <jasoninus.choi@gmail.com>
2019-08-07 18:34:26 +0000

    mod

--- dc/DC65_PrintNbyMInSpiral.java
+++ dc/DC65_PrintNbyMInSpiral.java
@@ -51,6 +51,7 @@ You should print out the following:
 14
 13
 12
+TC : O(N x M), SC : O(1)
  **/
 public class DC65_PrintNbyMInSpiral {
 	public static void printSpiral(int[][] arr) {
@@ -60,37 +61,65 @@ public class DC65_PrintNbyMInSpiral {
 		int rb = arr[0].length-1;
 		int r =0;
 		int c =0;
-		System.out.println( bb + ":" + rb);
-		while(ub<bb && lb<rb) {
-			while(c<rb) {
+		// @TODO : just check why total count is in need.
+		int total = arr.length*arr[0].length;
+		while(ub<=bb && lb<=rb && total>0) {
+			System.out.println( ub + " : " + bb + " : " + lb + " : " + rb);
+			while(c<rb && total>0) {
 				System.out.println(arr[r][c]);
 				c++;
+				total--;
 			}
 			ub++;
-			while(r<bb) {
+			while(r<bb && total>0) {
 				System.out.println(arr[r][c]);
 				r++;
+				total--;
 			}
 			rb--;
-			while(c>lb) {
+			while(c>lb && total>0) {
 				System.out.println(arr[r][c]);
 				c--;
+				total--;
 			}
 			bb--;
-			while(r>ub) {
+			while(r>ub && total>0) {
 				System.out.println(arr[r][c]);
 				r--;
+				total--;
 			}
 			lb++;
+		}
+
+		if(total>0) {
+			System.out.println(arr[r][c]);
+			total--;
 		}
 	}
 
 	public static void main(String[] args) {
-		int[][] arr = new int[][] {
+		int[][] arr2 = new int[][] {
 		 {1,  2,  3,  4,  5},
 		 {6,  7,  8,  9,  10},
 		 {11, 12, 13, 14, 15},
-		 {16, 17, 18, 19, 20}};
+		 {16, 17, 18, 19, 20},
+//		 {16, 17, 18, 19, 20},
+		 {26, 27, 28, 29, 30}};
+
+		int[][] arr = new int[][] {
+		 {1,  2,  3,  4,  5, 91 },
+		 {6,  7,  8,  9,  10, 92},
+		 {11, 12, 13, 14, 15, 93},
+		 {16, 17, 18, 19, 20, 94},
+		 {21, 22, 23, 24, 25, 95}};
+
+		for(int i=0; i<arr.length; i++) {
+			for(int j=0; j<arr[0].length; j++) {
+				System.out.printf("%3d", arr[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println();
 		printSpiral(arr);
 	}
 }
