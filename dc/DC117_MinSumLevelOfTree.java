fb0a93b128fba0e60dded3cd970002d04191c78a
freechelor <jasoninus.choi@gmail.com>
2019-08-13 18:52:16 +0000

    solved

--- dc/DC117_MinSumLevelOfTree.java
+++ dc/DC117_MinSumLevelOfTree.java
@@ -12,18 +12,62 @@ package alg.dc;
 
 import java.util.HashMap;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Collections;
 import java.util.Stack;
 import java.util.Queue;
 
+import alg.tr.TreeNode;
 /**
 DC#117_Easy
 This problem was asked by Facebook.
 
 Given a binary tree, return the level of the tree with minimum sum.
+
+			5
+		  /   \
+		 7     10
+	    / \   /
+	   2  4  6
+	  /
+	 1(O) 
+	==> 4
+
+			5
+		  /   \
+		 7     10
+	    / \   /
+	   2  4(O)  6
+	  /
+	 30 
+	 ==> 3
+
  **/
 public class DC117_MinSumLevelOfTree {
+	public static int level = 1;
+	public static int min = Integer.MAX_VALUE;
+	public static int sum(TreeNode root, int lev) {
+		if(root==null) return 0;
+		int self = root.value + sum(root.left, lev+1) + sum(root.right, lev+1);
+		if(min>self) { 
+			min = self;
+			level = lev;
+		}
+		return self;
+	}
+
+
 	public static void main(String[] args) {
+		TreeNode root = new TreeNode(5);
+		root.left = new TreeNode(3);
+		root.right = new TreeNode(6);
+		root.left.left = new TreeNode(2);
+		root.left.right = new TreeNode(41);
+		root.left.left.left = new TreeNode(30);
+		root.right.left = new TreeNode(-1);
+
+		sum(root, 1);
+		System.out.println("Level : " + level);
 	}
 }
