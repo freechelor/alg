456b101b89db9664dd408f16e3b49bdc4861aa69
freechelor <jasoninus.choi@gmail.com>
2019-08-13 15:36:28 +0000

    1st

---
+++ dc/DC117_MinSumLevelOfTree.java
@@ -0,0 +1,29 @@
+/**
+ * TODO : 
+ * 1. Time/Space Complexity Analysis
+ * 		BUT, MOST OF ALL, IT IS IMPORTANT TO 1.1. LIST POSSIBLE/AVAILABLE SOLUTIONS AND 1.2. THINK ABOUT COMPLEXITY FIRST AND IN ADVANCE,
+ * 		1.3. START WITH BEST COMPLEXITY SOLUTION
+ * 2. Improve Algorithm/Performance
+ * 3. Learn unfamiliar syntax 
+ * 4. Any other ways to consider + compare in terms of Time/Space complexity
+ *
+ */
+package alg.dc;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+import java.util.Stack;
+import java.util.Queue;
+
+/**
+DC#117_Easy
+This problem was asked by Facebook.
+
+Given a binary tree, return the level of the tree with minimum sum.
+ **/
+public class DC117_MinSumLevelOfTree {
+	public static void main(String[] args) {
+	}
+}
