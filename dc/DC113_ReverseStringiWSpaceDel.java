3e191edb8a563b3d226c44deb3605c6338ceb670
freechelor <jasoninus.choi@gmail.com>
2019-08-30 21:22:15 +0000

    1st

--- dc/DC113_ReverseStringiWSpaceDel.java
+++ dc/DC113_ReverseStringiWSpaceDel.java
@@ -26,6 +26,57 @@ Given a string of words delimited by spaces, reverse the words in string. For ex
 Follow-up: given a mutable string representation, can you perform this operation in-place?
  **/
 public class DC113_ReverseStringiWSpaceDel {
+	// we can scan string from the first and when we meet a space we can store a word we've been reading through
+	// we want to reverse the order of words, ie. a word we meet first has to be written later that we would meet later
+	// for the case, we could use Stack
+	//
+	// TODO : what about in-place reverse??
+	static Stack<String> s = new Stack<>();
+
+	public static void parseNStack(String stc) {
+		String word = null;
+		int prev = 0;
+		for(int i=0; i<stc.length(); i++) {
+			if(stc.charAt(i)==' ') { word = stc.substring(prev, i); s.push(word); prev=i;}
+		}
+		s.push(stc.substring(prev));
+	}
+
+	public static void reverse(String stc) {
+		parseNStack(stc);
+		while(!s.empty()) {
+			System.out.print(s.pop() + " ");
+		}
+	}
+
+	public static void swap(char[] c, int f, int l) {
+		char tmp = c[l];
+		c[l] = c[f];
+		c[f] = tmp;
+	}
+
+	public static char[] reverseInplace(char[] chars, int f, int l) {
+		while(f<l) {
+			swap(chars, f, l);
+			f++;
+			l--;
+		}
+		return chars;
+	}
+
+	public static char[] reversePart(char[] c, int s) {
+		int f = s;
+		int l = s;
+		while(c[s]!=' ' && s < c.length) {
+			s++;	
+		}
+		l = s-1;
+		reverseInplace(c, f, l);
+	}
+
 	public static void main(String[] args) {
+		reverse("Hello World here");
+		System.out.println();
+		System.out.println(reversePart(reverseInplace("Hellllllooooooo World Here")));
 	}
 }
