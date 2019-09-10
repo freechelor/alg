c317d90f19ef063f350e69a11d3e9a6e3f2a3245
freechelor <jasoninus.choi@gmail.com>
2019-09-10 20:17:25 +0000

    name changed

---
+++ dc/DC103_ShortString.java
@@ -0,0 +1,35 @@
+/**
+ * TODO : 
+ * 1. Time/Space Complexity Analysis
+ * 		BUT, MOST OF ALL, IT IS IMPORTANT TO 1.1. LIST POSSIBLE/AVAILABLE SOLUTIONS AND 1.2. THINK ABOUT COMPLEXITY FIRST AND IN ADVANCE,
+ * 		1.3. START WITH BEST COMPLEXITY SOLUTION
+ * 2. Improve Algorithm/Performance
+ * 3. Learn unfamiliar syntax 
+ * 4. Any other ways to consider + compare in terms of Time/Space complexity
+ *
+ */
+package alg.dc;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+import java.util.Stack;
+import java.util.Queue;
+/**
+DC#103_Medium
+_NOT_INVENT_CLUE
+This problem was asked by Square.
+
+Given a string and a set of characters, 
+return the shortest substring containing all the characters in the set.
+
+For example, given the string "figehaeci" and the set of characters {a, e, i}, 
+you should return "aeci".
+
+If there is no substring containing all the characters in the set, return null.
+ **/
+public class DC103_ShortString {
+	public static void main(String[] args) {
+	}
+}
