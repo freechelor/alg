010e4ee0ed720b07a16a3ea6a560f25f30ffebe8
freechelor <jasoninus.choi@gmail.com>
2019-09-11 21:06:32 +0000

    trybymyself

--- dc/DC103_ShortString.java
+++ dc/DC103_ShortString.java
@@ -69,14 +69,153 @@ public class DC103_ShortString {
 	public static int len(String s, char[] c) {
 		// the distance from the fist matching character to the second matching character	
 		// plus len(substring(index of the second matching charater), char[] except the first matching character)
+		return 0;
 	}
+
 
 	// make hashmap which has characters as keys and indices as values
 	// loop the given string when meet one of characters given, find other characters from hashmap and calculate distance, the sum of distances of farthest ones to the left and the right is the distance
 	// i.e.
 	// figehaeci, at index 3, on the left to the i distance is 2, on the right to the 
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		// a e i e a a e i i e a
 		//
+	}*/
+
+	static final int no_of_chars = 256; 
+	
+	// Function to find smallest window containing 
+	// all characters of 'pat' 
+	// THIS SOLUTION is NOT for the problem, but for similar one
+	static String findSubString(String str, String pat) { 
+		int len1 = str.length(); 
+		int len2 = pat.length(); 
+	
+		// check if string's length is less than pattern's 
+		// length. If yes then no such window can exist 
+		if (len1 < len2) 
+		{ 
+			System.out.println("No such window exists"); 
+			return ""; 
+		} 
+	
+		int hash_pat[] = new int[no_of_chars]; 
+		int hash_str[] = new int[no_of_chars]; 
+	
+		// store occurrence ofs characters of pattern 
+		for (int i = 0; i < len2; i++) 
+			hash_pat[pat.charAt(i)]++; 
+	
+		int start = 0, start_index = -1, min_len = Integer.MAX_VALUE; 
+	
+		// start traversing the string 
+		int count = 0; // count of characters 
+		for (int j = 0; j < len1 ; j++) 
+		{ 
+			// count occurrence of characters of string 
+			hash_str[str.charAt(j)]++; 
+	
+			// If string's char matches with pattern's char 
+			// then increment count 
+			if (hash_pat[str.charAt(j)] != 0 && 
+				hash_str[str.charAt(j)] <= hash_pat[str.charAt(j)] ) 
+				count++; 
+	
+			// if all the characters are matched 
+			if (count == len2) 
+			{ 
+				// Try to minimize the window i.e., check if 
+				// any character is occurring more no. of times 
+				// than its occurrence in pattern, if yes 
+				// then remove it from starting and also remove 
+				// the useless characters. 
+				while ( hash_str[str.charAt(start)] > hash_pat[str.charAt(start)] 
+					|| hash_pat[str.charAt(start)] == 0) 
+				{ 
+	
+					if (hash_str[str.charAt(start)] > hash_pat[str.charAt(start)]) 
+						hash_str[str.charAt(start)]--; 
+					start++; 
+				} 
+	
+				// update window size 
+				int len_window = j - start + 1; 
+				if (min_len > len_window) 
+				{ 
+					min_len = len_window; 
+					start_index = start; 
+				} 
+			} 
+		} 
+	
+		// If no window found 
+		if (start_index == -1) 
+		{ 
+		System.out.println("No such window exists"); 
+		return ""; 
+		} 
+	
+		// Return substring starting from start_index 
+		// and length min_len 
+		return str.substring(start_index, start_index + min_len); 
+	} 
+
+	// we will move from the first character of string to a next one.
+	// if the character matches one of characters given, we will set the index in another array int[]
+	// if all int[] is set, get value difference between the minimum index and the maximum index	
+	// and then, compare the value with the previous minimum value and if needed update
+	// then, move to next in the string and update index if the character matches with one in char[] array
+	// then, calculate the length again
+	static HashMap<Character, Integer> map = new HashMap<>();
+	static boolean hasSolution = true;
+	public static String shortestString(String org, char[] c) {
+		int[] flag = new int[c.length];
+		int count = 0;
+		int[] charset = new int[26];
+		int shortest = Integer.MAX_VALUE;
+		int start = 0;
+		int end = 0;
+		for(int i=0; i<c.length; i++) {
+			charset[c[i]-'a'] = 1;
+		}
+		for(int i=0; i < org.length(); i++) {
+			if(charset[org.charAt(i)-'a']==1) {
+				map.put(new Character(org.charAt(i)), i);
+				int min = Integer.MAX_VALUE;
+				int max = Integer.MIN_VALUE;
+				for(int j=0; j<c.length; j++) {
+					int tmp = map.getOrDefault(c[j], -1);
+					if(tmp!=-1) {
+						hasSolution = true;
+						if(tmp<min) min = tmp;
+						if(tmp>max) max = tmp;
+					} else {
+						hasSolution = false;
+						break;
+					}
+				}
+				if(hasSolution) {
+					if(shortest>max-min) { 
+						shortest = max-min;
+						start = min;
+						end = max;
+						System.out.println("min : " + min + " , max : " + max);
+					}
+				}
+			}
+		}
+		return org.substring(start, end+1);
 	}
+
+	// Driver Method 
+	public static void main(String[] args) { 
+		String str = "this is a test string"; 
+		String pat = "tist"; 
+	
+		System.out.println("Smallest window is :\n " + 
+						findSubString(str, pat)); 
+		String str2 = "figehaecawouthbowdheslbieoahwakleiqtehdkhgioeni";
+		//String str2 = "figehaeci";
+		System.out.println(shortestString(str2, new char[] {'a','e','i'}));
+	} 
 }
