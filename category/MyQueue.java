e3ddc15129512f09f53a18a0c7b3ffec7cc6301b
freechelor <jasoninus.choi@gmail.com>
2019-12-11 05:38:40 +0000

    mod

--- category/MyQueue.java
+++ category/MyQueue.java
@@ -18,14 +18,14 @@ import java.util.Stack;
 import java.util.Queue;
 
 // TODO : Consider Generic
-public class MyQueue {
+public class MyQueue<T> {
 	// 1. using linkedlist
 	// 		have to implement my own linkedlist
-	private Node first = null;
-	private Node last = null;
+	private Node<T> first = null;
+	private Node<T> last = null;
 	private int count = 0;
-	public void add(Object obj) {
-		Node n = new Node(obj);
+	public void add(T obj) {
+		Node<T> n = new Node(obj);
 		if(last==null) {
 			first = n;
 			last = n;
@@ -39,9 +39,9 @@ public class MyQueue {
 		last = n;
 	}	
 
-	public Object remove() {
+	public T remove() {
 		if(first==null) return null;
-		Object tmp = first.obj;
+		T tmp = first.obj;
 		first = first.next;
 		count--;
 		return tmp;
@@ -54,27 +54,67 @@ public class MyQueue {
 
 
 	// 2. using stack
+	private Stack stack = new Stack();
+	private Stack stack2 = new Stack();
+	public void add2(Object obj) {
+		stack.push(obj);
+	}
+
+	public Object remove2() {
+		if(stack2.isEmpty()) {
+			if(stack.isEmpty()) return null;
+			while(!stack.isEmpty()) {
+				stack2.push(stack.pop());
+			}
+			return stack2.pop();
+		}
+		return stack2.pop();
+	}
+
+	public int size2() {
+		return stack.size() + stack2.size();
+	}
+
 	public static void main(String[] args) {
-		MyQueue mQ = new MyQueue();
-		mQ.add("1");
-		mQ.add("2");
-		mQ.add("3");
-		mQ.add("4");
-		mQ.add("5");
-		System.out.println(mQ.remove());
-		System.out.println(mQ.remove());
-		System.out.println(mQ.remove());
-		System.out.println(mQ.remove());
-		System.out.println(mQ.remove());
-		System.out.println(mQ.remove());
+		MyQueue<Integer> mQ = new MyQueue<>();
+		mQ.add(1);
+		mQ.add(2);
+		mQ.add(3);
+		mQ.add(4);
+		mQ.add(5);
+		System.out.println(mQ.remove() + ":" + mQ.size());
+		mQ.add(6);
+		mQ.add(7);
+		System.out.println(mQ.remove() + ":" + mQ.size());
+		System.out.println(mQ.remove() + ":" + mQ.size());
+		System.out.println(mQ.remove() + ":" + mQ.size());
+		System.out.println(mQ.remove() + ":" + mQ.size());
+		System.out.println(mQ.remove() + ":" + mQ.size());
+		System.out.println(mQ.remove() + ":" + mQ.size());
+		System.out.println(mQ.remove() + ":" + mQ.size());
+		mQ.add2(1);
+		mQ.add2(2);
+		mQ.add2(3);
+		mQ.add2(4);
+		mQ.add2(5);
+		System.out.println(mQ.remove2() + ":" + mQ.size2());
+		System.out.println(mQ.remove2() + ":" + mQ.size2());
+		System.out.println(mQ.remove2() + ":" + mQ.size2());
+		mQ.add2(6);
+		mQ.add2(7);
+		System.out.println(mQ.remove2() + ":" + mQ.size2());
+		System.out.println(mQ.remove2() + ":" + mQ.size2());
+		System.out.println(mQ.remove2() + ":" + mQ.size2());
+		System.out.println(mQ.remove2() + ":" + mQ.size2());
+		System.out.println(mQ.remove2() + ":" + mQ.size2());
 	}
 }
 
-class Node {
-	public Node next;
-	public Object obj;
+class Node<T> {
+	public Node<T> next;
+	public T obj;
 
-	public Node(Object obj) {
+	public Node(T obj) {
 		this.obj = obj;
 	}
 }
