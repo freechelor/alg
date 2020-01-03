7c318e5db61c2f2d019885d67e31f58a96561c4f
freechelor <jasoninus.choi@gmail.com>
2020-01-03 21:23:25 +0000

    1st

---
+++ stx/SelfFill.java
@@ -0,0 +1,36 @@
+package alg.stx;
+
+import java.util.HashMap;
+
+public class SelfFill {
+	static HashMap<String, SelfFill> map = new HashMap<>();
+	private String key;
+	private String value;
+
+
+	public SelfFill(String k) {
+		key = k;
+	}
+
+	public boolean get() {
+		if(map.get(this.key)!=null) {
+			this.value = "awesome";
+			return true;
+		}
+		return false;
+	}
+
+	public static void main(String args[]) {
+		SelfFill sf = new SelfFill("k1");
+		SelfFill sf2 = new SelfFill("k2");
+		SelfFill sf3 = new SelfFill("k3");
+		map.put("k1", sf); 		
+		map.put("k3", sf3); 		
+		sf.get();
+		sf2.get();
+		sf3.get();
+		System.out.println(sf.key + " : " + sf.value);
+		System.out.println(sf2.key + " : " + sf2.value);
+		System.out.println(sf3.key + " : " + sf3.value);
+	}
+}
