0a53d8d073c89ef10f0033628ddac875df3baee6
freechelor <jasoninus.choi@gmail.com>
2020-01-08 22:16:50 +0000

    problem

---
+++ dc/DC266.java
@@ -0,0 +1,32 @@
+/**
+ * TODO : 
+ * 1. Time/Space Complexity Analysis
+ * 		BUT, MOST OF ALL, IT IS IMPORTANT TO 1.1. LIST POSSIBLE/AVAILABLE SOLUTIONS AND 1.2. THINK ABOUT COMPLEXITY FIRST AND IN ADVANCE,
+ * 		1.3. START WITH BEST COMPLEXITY SOLUTION
+ * 2. Improve Algorithm/Performance
+ * 3. Learn unfamiliar syntax 
+ * 4. Any other ways to consider + compare in terms of Time/Space complexity
+ *
+ */
+package alg.dc;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Collections;
+import java.util.Stack;
+import java.util.Queue;
+/**
+ *
+This problem was asked by Pivotal.
+
+A step word is formed by taking a given word, adding a letter, and anagramming the result. 
+For example, starting with the word "APPLE", you can add an "A" and anagram to get "APPEAL".
+
+Given a dictionary of words and an input word, create a function that returns all valid step words.
+ */
+public class DC266 {
+	public static void main(String[] args) {
+	}
+}
